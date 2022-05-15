21ccf33fa458ff1707f0f991b765f85ff52430f7
Lee Min Wook <dnr14@naver.com>
2022-05-15 17:10:36 +0900

    feat : json server 이용하기 위한 초기 세팅

---
+++ src/api/http.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosError } from 'axios';
+import { HTTPMethod } from 'types/http';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  timeout: 10000,
+});
+
+axiosInstance.interceptors.response.use(
+  response => response,
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+const http: HTTPMethod = {
+  get: async (url, config) => {
+    try {
+      const res = await axiosInstance.get(url, config);
+      return res.data.response;
+    } catch (e) {
+      throw errorHandler(e);
+    }
+  },
+  delete: async (url, config) => {
+    try {
+      const res = await axiosInstance.delete(url, config);
+      return res.data.response;
+    } catch (e) {
+      throw errorHandler(e);
+    }
+  },
+  post: async (url, body, config) => {
+    try {
+      const res = await axiosInstance.post(url, body, config);
+      return res.data.response;
+    } catch (e) {
+      throw errorHandler(e);
+    }
+  },
+  put: async (url, body, config) => {
+    try {
+      const res = await axiosInstance.put(url, body, config);
+      return res.data.response;
+    } catch (e) {
+      throw errorHandler(e);
+    }
+  },
+};
+export const errorHandler = (error: unknown) => {
+  if (axios.isAxiosError(error)) {
+    return new Error(error.message);
+  }
+  return error;
+};
+
+export default http;
---
+++ src/types/http.d.ts
@@ -0,0 +1,11 @@
+import { AxiosRequestConfig } from 'axios';
+
+type GetOrDelete = <T>(url: string, config?: AxiosRequestConfig) => Promise<T>;
+type PostOrPut = <P, R = void>(url: string, body?: P, config?: AxiosRequestConfig) => Promise<R>;
+
+export interface HTTPMethod {
+  get: GetOrDelete;
+  delete: GetOrDelete;
+  post: PostOrPut;
+  put: PostOrPut;
+}
